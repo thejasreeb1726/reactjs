0c45582b2d96535c16a549e39f9702a3ccf24e22
Thejasree B <165803883+thejasreeb1726@users.noreply.github.com>
2025-07-29 22:24:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState, useEffect } from "react";
+// import { Button } from 'antd';
+import "antd/dist/reset.css";
+import "./App.css";
+// import Usercard from "./components/userCard.js";
+import UserList from "./components/userList.js";
+
+async function getUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+}
+export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    getUsers()
+      .then((res) => {
+        console.log(res);
+        setUsers(res);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <div>Loading....</div>
+      ) : users.length > 0 ? (
+        <UserList users={users} />
+      ) : (
+        <div>NO users</div>
+      )}
+    </div>
+  );
+}
